498bdb21c8123650d38e538259b374304a951083
v16s <shankar.vignesh@gmail.com>
2018-09-13 15:22:55 +0530

    minor tweaks

--- src/front/components/AddQuestion.js
+++ src/front/components/AddQuestion.js
@@ -26,213 +26,249 @@ class AddQuestion extends React.Component {
     console.log(e)
   }
   render () {
-    return (<Grid.Column>
+    return (
+      <Grid.Column>
         <Form>
-        <Segment>
-        <Segment basic>
-            <Form.Field>
-                <label>Select Category</label>
-              <Dropdown fluid
-                placeholder='Select Category'
-                selection
-                className='category-select'
-                options={[
-                  {
-                    text: 'Category 1',
-                    value: 'c1'
-                  },
-                  {
-                    text: 'Category 2',
-                    value: 'c2'
-                  },
-                  {
-                    text: 'Category 3',
-                    value: 'c3'
-                  }
-                ]}
-              />
-              </Form.Field>
-          </Segment>
-          <Segment basic>
-            <Form.Field>
-                <label>Question Name</label>
-                <Input fluid size='large' placeholder='Question Name'>
-              <input/>
-             </Input></Form.Field>
-            </Segment>
-
-          <Segment basic><Form.Field>
-          <label>Question Description</label>
-            <Input fluid size='large' placeholder='Question Description' onChange={(e) => {this.handleChange(e)}}>
-              <input/>
-             </Input></Form.Field>
-            </Segment>
-
-
-
-                 <Segment basic>
-             <Input fluid size='large' placeholder='Question Name'>
-              <input/>
-             <Button
-                primary
-                style={{
-                  borderTopLeftRadius: '0px',
-                  borderBottomLeftRadius: '0px'
-                }}
-              >
-                <Icon
-                  name='add'
-                  style={{
-                    margin: '0',
-                    opacity: '1'
-                  }}
-                />
-              </Button>
-                  </Input>
-          </Segment>
-          <Segment basic>
-          <Form.Field>
-         <label>Equation</label>
-         <Input fluid size="large">
-         <input/>
-         <Button
-                primary
-                content='Add Equation'
-                style={{
-                  borderTopLeftRadius: '0px',
-                  borderBottomLeftRadius: '0px'
-                }}
-              />
-               <Button
-                primary
-                content='Latex Guide'
-            />          
-         </Input>
-         </Form.Field>
-         </Segment>
-         <Segment basic>
-         <Grid columns={2} divided>
-    <Grid.Row>
-      <Grid.Column>
-          <Grid column={2}>
-            <Grid.Column>
-             <Form.Radio/>
-             </Grid.Column>
-             <Grid.Column className='input-column'>
-             <Input placeholder='Search...' size='small'/>
-            </Grid.Column>
-          </Grid>
-      </Grid.Column>
-      <Grid.Column>
-      <Grid column={2}>
-            <Grid.Column>
-             <Form.Radio/>
-             </Grid.Column>
-             <Grid.Column className='input-column'>
-             <Input placeholder='Search...' size='small'/>
-            </Grid.Column>
-          </Grid>
-      </Grid.Column>
-     </Grid.Row>
-<Grid.Row>
-      <Grid.Column>
-      <Grid column={2}>
-            <Grid.Column>
-             <Form.Radio/>
-             </Grid.Column>
-             <Grid.Column className='input-column'>
-             <Input placeholder='Search...' size='small'/>
-            </Grid.Column>
-          </Grid>
-      </Grid.Column>
-      <Grid.Column>
-      <Grid column={2}>
-            <Grid.Column>
-             <Form.Radio/>
-             </Grid.Column>
-             <Grid.Column className='input-column'>
-             <Input placeholder='Search...' size='small'/>
-            </Grid.Column>
-          </Grid>
-      </Grid.Column>
-      </Grid.Row>
-  </Grid>
-
-  <Segment basic>
-  <Form>
-    <Form.Group widths='equal'>
-    <Form.Field label='Select Level' control='select'>
-        <option value='male'>Male</option>
-        <option value='female'>Female</option>
-      </Form.Field>
-      <Form.Field label='File Upload' control='input' />  
-    </Form.Group>
-    </Form>
-          </Segment>
           <Segment>
-          <Grid columns={4} divided>
-    <Grid.Row>
-      <Grid.Column>
-      <Form>
-    <Form.Field label='Exam' control='select'>
-        <option value='male'>Male</option>
-        <option value='female'>Female</option>
-      </Form.Field>
-    </Form>
-      </Grid.Column>
-      <Grid.Column>
-      <Form>
-    <Form.Field label='Company' control='select'>
-        <option value='male'>Male</option>
-        <option value='female'>Female</option>
-      </Form.Field>
-    </Form>
-      </Grid.Column>
-      <Grid.Column>
-      <Form>
-    <Form.Field label='Skill' control='select'>
-        <option value='male'>Male</option>
-        <option value='female'>Female</option>
-      </Form.Field>
-    </Form>
-      </Grid.Column>
-      <Grid.Column>
-      <Form>
-    <Form.Field label='Topic' control='select'>
-        <option value='male'>Male</option>
-        <option value='female'>Female</option>
-      </Form.Field>
-    </Form>
-      </Grid.Column>
-    </Grid.Row>
-    </Grid>
-    <Segment basic>
-            <Form.Field>
-                <Input fluid size='large'>
-              <input/>
-             </Input></Form.Field>
+            <Segment basic>
+              <Form.Field>
+                <label>Select Category</label>
+                <Dropdown
+                  fluid
+                  placeholder='Select Category'
+                  selection
+                  className='category-select'
+                  options={[
+                    {
+                      text: 'Category 1',
+                      value: 'c1'
+                    },
+                    {
+                      text: 'Category 2',
+                      value: 'c2'
+                    },
+                    {
+                      text: 'Category 3',
+                      value: 'c3'
+                    }
+                  ]}
+                />
+              </Form.Field>
             </Segment>
             <Segment basic>
-            <Form.Field>
-                <label>Hints for description</label>
+              <Form.Field>
+                <label>Question Name</label>
                 <Input fluid size='large' placeholder='Question Name'>
-              <input/>
-             </Input></Form.Field>
+                  <input />
+                </Input>
+              </Form.Field>
             </Segment>
 
-             <Button fluid color='green'>Add Question</Button>
+            <Segment basic>
+              <Form.Field>
+                <label>Question Description</label>
+                <Input
+                  fluid
+                  size='large'
+                  placeholder='Question Description'
+                  onChange={e => {
+                    this.handleChange(e)
+                  }}
+                >
+                  <input />
+                </Input>
+              </Form.Field>
+            </Segment>
 
+            <Segment basic>
+              <Input fluid size='large' placeholder='Question Name'>
+                <input />
+                <Button
+                  primary
+                  style={{
+                    borderTopLeftRadius: '0px',
+                    borderBottomLeftRadius: '0px'
+                  }}
+                >
+                  <Icon
+                    name='add'
+                    style={{
+                      margin: '0',
+                      opacity: '1'
+                    }}
+                  />
+                </Button>
+              </Input>
+            </Segment>
+            <Segment basic>
+              <Form.Field>
+                <label>Equation</label>
+                <Input fluid size='large'>
+                  <input />
+                  <Button
+                    primary
+                    content='Add Equation'
+                    style={{
+                      borderTopLeftRadius: '0px',
+                      borderBottomLeftRadius: '0px'
+                    }}
+                  />
+                  <Button primary content='Latex Guide' />
+                </Input>
+              </Form.Field>
+            </Segment>
+
+            <Segment basic>
+              <Form.Field>
+                <Grid columns={2} divided>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Grid column={2}>
+                        <Grid.Column>
+                          <Form.Radio />
+                        </Grid.Column>
+                        <Grid.Column className='input-column'>
+                          <Input placeholder='Search...' size='small' />
+                        </Grid.Column>
+                      </Grid>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Grid column={2}>
+                        <Grid.Column>
+                          <Form.Radio />
+                        </Grid.Column>
+                        <Grid.Column className='input-column'>
+                          <Input placeholder='Search...' size='small' />
+                        </Grid.Column>
+                      </Grid>
+                    </Grid.Column>
+                  </Grid.Row>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Grid column={2}>
+                        <Grid.Column>
+                          <Form.Radio />
+                        </Grid.Column>
+                        <Grid.Column className='input-column'>
+                          <Input placeholder='Search...' size='small' />
+                        </Grid.Column>
+                      </Grid>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Grid column={2}>
+                        <Grid.Column>
+                          <Form.Radio />
+                        </Grid.Column>
+                        <Grid.Column className='input-column'>
+                          <Input placeholder='Search...' size='small' />
+                        </Grid.Column>
+                      </Grid>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Form.Field>
+              <Segment basic>
+                <Form.Group widths='equal'>
+                  <Dropdown
+                    fluid
+                    placeholder='Select Category'
+                    selection
+                    className='category-select'
+                    options={[
+                      { text: 'Hello', value: 1 },
+                      { text: 'Hello', value: 2 },
+                      { text: 'Hello', value: 3 }
+                    ]}
+                  />
+                  <Form.Field>
+                    <Input fluid size='large'>
+                      <input type='file' />
+                    </Input>
+                  </Form.Field>
+                </Form.Group>
+              </Segment>
+              <Segment>
+                <Grid columns={4} divided>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Dropdown
+                        fluid
+                        placeholder='Select Category'
+                        selection
+                        className='category-select'
+                        options={[
+                          { text: 'Hello', value: 1 },
+                          { text: 'Hello', value: 2 },
+                          { text: 'Hello', value: 3 }
+                        ]}
+                      />
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Dropdown
+                        fluid
+                        placeholder='Select Category'
+                        selection
+                        className='category-select'
+                        options={[
+                          { text: 'Hello', value: 1 },
+                          { text: 'Hello', value: 2 },
+                          { text: 'Hello', value: 3 }
+                        ]}
+                      />
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Dropdown
+                        fluid
+                        placeholder='Select Category'
+                        selection
+                        className='category-select'
+                        options={[
+                          { text: 'Hello', value: 1 },
+                          { text: 'Hello', value: 2 },
+                          { text: 'Hello', value: 3 }
+                        ]}
+                      />
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Dropdown
+                        fluid
+                        placeholder='Select Category'
+                        selection
+                        className='category-select'
+                        options={[
+                          { text: 'Hello', value: 1 },
+                          { text: 'Hello', value: 2 },
+                          { text: 'Hello', value: 3 }
+                        ]}
+                      />
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+                <Segment basic>
+                  <Form.Field>
+                    <Input fluid size='large'>
+                      <input />
+                    </Input>
+                  </Form.Field>
+                </Segment>
+                <Segment basic>
+                  <Form.Field>
+                    <label>Hints for description</label>
+                    <Input fluid size='large' placeholder='Question Name'>
+                      <input />
+                    </Input>
+                  </Form.Field>
+                </Segment>
+
+                <Button fluid color='green'>Add Question</Button>
+
+              </Segment>
+            </Segment>
 
           </Segment>
-             </Segment>
-             
-        </Segment>
-       
+
         </Form>
       </Grid.Column>
-      
-
-      
     )
   }
 }
