de80b14e21bc8a78c52be72a3fb75dcdd8dfc36c
v16s <shankar.vignesh@gmail.com>
2018-09-21 17:38:50 +0530

    making more of addQuestion functional

--- src/front/components/AddQuestion.js
+++ src/front/components/AddQuestion.js
@@ -21,22 +21,26 @@ import Answers from './Answers'
 import Preview from './Preview'
 import Select from 'react-select'
 import Spinner from 'react-spinkit'
-import makeAnimated from 'react-select/lib/animated';
+import makeAnimated from 'react-select/lib/animated'
+import Latex from 'react-latex'
 class AddQuestion extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       categories: props.categories,
       selCat: null,
+      selTopic: null,
       previewData: 'Question Description with an equation: $x^2 + 2x = 0$',
       name: 'Question Name',
       value: '',
       options: {
         a: 'Option A',
-      b: 'Option B',
-      c: 'Option C',
-      d: 'Option D'
-    }
+        b: 'Option B',
+        c: 'Option C',
+        d: 'Option D'
+      },
+      topics: [],
+      hints: 'A hint for your question'
     }
     this.catSelector = React.createRef()
     this.qname = React.createRef()
@@ -50,93 +54,138 @@ class AddQuestion extends React.Component {
     this.handlePreviewChange = this.handlePreviewChange.bind(this)
     this.handleRadio = this.handleRadio.bind(this)
     this.handleAnswer = this.handleAnswer.bind(this)
+    this.handleCategoryChange = this.handleCategoryChange.bind(this)
+    this.handleTopicChange = this.handleTopicChange.bind(this)
+    this.handleLevelChange = this.handleLevelChange.bind(this)
+    this.handleSkillChange = this.handleSkillChange.bind(this)
+    this.handleCompanyChange = this.handleCompanyChange.bind(this)
+    this.handleExamChange = this.handleExamChange.bind(this)
+    this.handleHintsChange = this.handleHintsChange.bind(this)
+    this.handleTagTopicChange = this.handleTagTopicChange.bind(this)
   }
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate (nextProps, nextState) {
     nextState.categories = nextProps.categories
 
     return true
   }
-  componentDidMount() {
-    
-    
-  }
+  componentDidMount () {}
   handleChange (e) {
     console.log({
-      category: this.catSelector.current.state.value.label,
-      qname: this.qname.current.inputRef.value,
-      qdef: this.qdef.current.inputRef.value,
+      category: this.state.selCat,
+      qname: this.state.name,
+      qdef: this.state.previewData,
       exams: this.exams.current.state.value,
+      options: this.state.options,
+      answer: this.state.value
     })
   }
   handleRadio (e, { value }) {
     this.setState({ value })
   }
-  handlePreviewChange(e) {
-    this.setState({previewData: e.target.value})
+  handlePreviewChange (e) {
+    this.setState({ previewData: e.target.value })
   }
-  handleNameChange(e) {
-    this.setState({name: e.target.value})
+  handleNameChange (e) {
+    this.setState({ name: e.target.value })
   }
-  handleAnswer(e, opt) {
-    let {options} = this.state
+  handleAnswer (e, opt) {
+    let { options } = this.state
     options[opt] = e.target.value
-    this.setState({options: options})
+    this.setState({ options: options })
   }
+  handleCategoryChange (e) {
+    this.setState({ topics: e.topics, selCat: { _id: e.value, name: e.label } })
+  }
+  handleTopicChange (e) {
+    this.setState({ selTopic: { _id: e.value, name: e.label } })
+  }
+  handleLevelChange (e) {}
+  handleExamChange (e) {}
+  handleSkillChange (e) {}
+  handleTagTopicChange (e) {}
+  handleHintsChange (e) {
+    this.setState({ hints: e.target.value })
+  }
+  handleCompanyChange (e) {}
   render () {
-    let {value} = this.state
+    let { value, topics } = this.state
+    let { categories } = this.props
     return (
       <Grid.Column>
         <Form onSubmit={this.handleChange}>
           <Segment>
             <Segment basic>
-              <Form.Field required>
-                <label>Select Category</label>
-                <Select
-                ref={this.catSelector}
-                components={makeAnimated()}
-                  options={[
-                    {
-                      label: 'Category 1',
-                      value: 'c1'
-                    },
-                    {
-                      label: 'Category 2',
-                      value: 'c2'
-                    },
-                    {
-                      label: 'Category 3',
-                      value: 'c3'
-                    }
-                  ]}
-                />
-              </Form.Field>
+              <Form.Group widths='equal'>
+                <Form.Field>
+                  <label>Select Category</label>
+                  <Select
+                    className='category-select'
+                    ref={this.catSelector}
+                    components={makeAnimated()}
+                    options={_.map(categories, k => {
+                      return { value: k._id, label: k.name, topics: k.topics }
+                    })}
+                    onChange={this.handleCategoryChange}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Select Topic</label>
+                  <Select
+                    className='category-select'
+                    ref={this.catSelector}
+                    components={makeAnimated()}
+                    options={_.map(topics, k => {
+                      return { value: k.id, label: k.name }
+                    })}
+                    isDisabled={topics.length == 0}
+                    onChange={this.handleTopicChange}
+                  />
+                </Form.Field>
+              </Form.Group>
             </Segment>
             <Segment basic>
               <Form.Field>
                 <label>Question Name</label>
-                <Input onChange={this.handleNameChange} ref={this.qname} fluid size='large' placeholder='Question Name'>
+                <Input
+                  onChange={this.handleNameChange}
+                  ref={this.qname}
+                  fluid
+                  size='large'
+                  placeholder='Question Name'
+                  required
+                >
                   <input />
                 </Input>
               </Form.Field>
             </Segment>
 
-           <Segment basic style={{
-             display: 'flex',
-             flexDirection: 'column'
-           }}>
+            <Segment
+              basic
+              style={{
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
               <Form.Field>
                 <label>Question Description</label>
-                <Input fluid size='large' >
-                  <textarea onChange={this.handlePreviewChange} placeholder="Question Description with an equation: $x^2 + 2x = 0$"/>
-                  
-                  
+                <Input fluid size='large' required>
+                  <textarea
+                    onChange={this.handlePreviewChange}
+                    placeholder='Question Description with an equation: $x^2 + 2x = 0$'
+                  />
+
                 </Input>
               </Form.Field>
-              <Button fluid basic primary content='Latex Guide' style={{alignSelf: 'flex-end'}}/>
+              <Button
+                fluid
+                basic
+                primary
+                content='Latex Guide'
+                style={{ alignSelf: 'flex-end' }}
+              />
             </Segment>
 
-
-              <Preview desc={this.state.previewData} name={this.state.name}/>
+            <Preview desc={this.state.previewData} name={this.state.name} />
             <Segment basic>
               <Form.Field>
                 <Grid columns={2} divided>
@@ -145,27 +194,37 @@ class AddQuestion extends React.Component {
                       <Grid column={2}>
                         <Grid.Column className='radio-column'>
                           <Form.Radio
-                          value='a'
-                          checked={value == 'a'}
-                          onChange={this.handleRadio}
-                           />
+                            value='a'
+                            checked={value == 'a'}
+                            onChange={this.handleRadio}
+                          />
                         </Grid.Column>
                         <Grid.Column className='input-column'>
-                          <Input placeholder='Option A' size='small' onChange={e => this.handleAnswer(e, 'a')} />
+                          <Input
+                            required
+                            placeholder='Option A'
+                            size='small'
+                            onChange={e => this.handleAnswer(e, 'a')}
+                          />
                         </Grid.Column>
                       </Grid>
                     </Grid.Column>
                     <Grid.Column>
                       <Grid column={2}>
                         <Grid.Column className='radio-column'>
-                          <Form.Radio 
+                          <Form.Radio
                             value='b'
-                          checked={value == 'b'}
-                          onChange={this.handleRadio}
+                            checked={value == 'b'}
+                            onChange={this.handleRadio}
                           />
                         </Grid.Column>
                         <Grid.Column className='input-column'>
-                          <Input placeholder='Option B' size='small' onChange={e => this.handleAnswer(e, 'b')} />
+                          <Input
+                            required
+                            placeholder='Option B'
+                            size='small'
+                            onChange={e => this.handleAnswer(e, 'b')}
+                          />
                         </Grid.Column>
                       </Grid>
                     </Grid.Column>
@@ -174,28 +233,38 @@ class AddQuestion extends React.Component {
                     <Grid.Column>
                       <Grid column={2}>
                         <Grid.Column className='radio-column'>
-                          <Form.Radio 
+                          <Form.Radio
                             value='c'
-                          checked={value == 'c'}
-                          onChange={this.handleRadio}
+                            checked={value == 'c'}
+                            onChange={this.handleRadio}
                           />
                         </Grid.Column>
                         <Grid.Column className='input-column'>
-                          <Input placeholder='Option C' size='small' onChange={e => this.handleAnswer(e, 'c')} />
+                          <Input
+                            required
+                            placeholder='Option C'
+                            size='small'
+                            onChange={e => this.handleAnswer(e, 'c')}
+                          />
                         </Grid.Column>
                       </Grid>
                     </Grid.Column>
                     <Grid.Column>
                       <Grid column={2}>
                         <Grid.Column className='radio-column'>
-                          <Form.Radio 
+                          <Form.Radio
                             value='d'
-                          checked={value == 'd'}
-                          onChange={this.handleRadio}
+                            checked={value == 'd'}
+                            onChange={this.handleRadio}
                           />
                         </Grid.Column>
                         <Grid.Column className='input-column'>
-                          <Input placeholder='Option D' size='small' onChange={e => this.handleAnswer(e, 'd')} />
+                          <Input
+                            required
+                            placeholder='Option D'
+                            size='small'
+                            onChange={e => this.handleAnswer(e, 'd')}
+                          />
                         </Grid.Column>
                       </Grid>
                     </Grid.Column>
@@ -208,6 +277,7 @@ class AddQuestion extends React.Component {
                   <Dropdown
                     fluid
                     placeholder='Select Level'
+                    required
                     selection
                     className='category-select'
                     options={[
@@ -227,113 +297,111 @@ class AddQuestion extends React.Component {
                 <Grid columns={4} divided>
                   <Grid.Row>
                     <Grid.Column>
-                    <Select
-                    placeholder='Exams'
-                    isMulti
-                ref={this.exams}
-                components={makeAnimated()}
+                      <Select
+                        placeholder='Exams'
+                        isMulti
+                        ref={this.exams}
+                        components={makeAnimated()}
                         options={[
-                    {
-                      label: 'Exam 1',
-                      value: 'e1'
-                    },
-                    {
-                      label: 'Exam 2',
-                      value: 'e2'
-                    },
-                    {
-                      label: 'Exam 3',
-                      value: 'e3'
-                    }
+                          {
+                            label: 'Exam 1',
+                            value: 'e1'
+                          },
+                          {
+                            label: 'Exam 2',
+                            value: 'e2'
+                          },
+                          {
+                            label: 'Exam 3',
+                            value: 'e3'
+                          }
                         ]}
                       />
                     </Grid.Column>
                     <Grid.Column>
-                    <Select
-                    isMulti
-                ref={this.company}
-                components={makeAnimated()}
-                placeholder='Companies'
+                      <Select
+                        isMulti
+                        ref={this.company}
+                        components={makeAnimated()}
+                        placeholder='Companies'
                         options={[
-                    {
-                      label: 'Company 1',
-                      value: 'cm1'
-                    },
-                    {
-                      label: 'Company 2',
-                      value: 'cm2'
-                    },
-                    {
-                      label: 'Company 3',
-                      value: 'cm3'
-                    }
+                          {
+                            label: 'Company 1',
+                            value: 'cm1'
+                          },
+                          {
+                            label: 'Company 2',
+                            value: 'cm2'
+                          },
+                          {
+                            label: 'Company 3',
+                            value: 'cm3'
+                          }
                         ]}
                       />
                     </Grid.Column>
                     <Grid.Column>
-                    <Select
-                    isMulti
-                ref={this.skill}
-                components={makeAnimated()}
-                placeholder='Skills'
+                      <Select
+                        isMulti
+                        ref={this.skill}
+                        components={makeAnimated()}
+                        placeholder='Skills'
                         options={[
-                    {
-                      label: 'Skill 1',
-                      value: 's1'
-                    },
-                    {
-                      label: 'Skill 2',
-                      value: 's2'
-                    },
-                    {
-                      label: 'Skill 3',
-                      value: 's3'
-                    }
+                          {
+                            label: 'Skill 1',
+                            value: 's1'
+                          },
+                          {
+                            label: 'Skill 2',
+                            value: 's2'
+                          },
+                          {
+                            label: 'Skill 3',
+                            value: 's3'
+                          }
                         ]}
                       />
                     </Grid.Column>
                     <Grid.Column>
-                    <Select
-                    isMulti
-                ref={this.topic}
-                components={makeAnimated()}
-                placeholder='Topics'
-                        options={[
-                    {
-                      label: 'Topic 1',
-                      value: 't1'
-                    },
-                    {
-                      label: 'Topic 2',
-                      value: 't2'
-                    },
-                    {
-                      label: 'Topic 3',
-                      value: 't3'
-                    }
-                        ]}
+                      <Select
+                        isMulti
+                        ref={this.topic}
+                        components={makeAnimated()}
+                        placeholder='Topics'
+                        options={_.map(topics, k => {
+                          return {
+                            value: k.id,
+                            label: k.name
+                          }
+                        })}
+                        onChange={this.handleTagTopicChange}
                       />
                     </Grid.Column>
                   </Grid.Row>
                 </Grid>
-               
-               
 
-                
               </Segment>
-             
-                  <Form.Field>
-                    <label>Hints for description</label>
-                    <Input fluid size='large' placeholder='Question Name'>
-                      <input />
-                      
-                    </Input>
-                  </Form.Field>
-                
-              <Segment></Segment>
-              <Segment basic>
+
+              <Form.Field>
+                <label>Hints for description</label>
+                <Input
+                  fluid
+                  size='large'
+                  placeholder='A hint for your question'
+                  onChange={this.handleHintsChange}
+                  required
+                >
+                  <input />
+
+                </Input>
+              </Form.Field>
+
+              <Segment>
+                <b>Hint:</b> <Latex>{this.state.hints}</Latex>
+              </Segment>
+
               <Form.Button fluid primary>Add Question</Form.Button>
-              </Segment>
+
             </Segment>
 
           </Segment>
--- src/front/components/Answers.js
+++ src/front/components/Answers.js
@@ -1,50 +1,73 @@
-import React, { Component } from 'react';
-import {Form, Grid, Segment, Input} from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Form, Grid, Segment, Input } from 'semantic-ui-react'
+import Latex from 'react-latex'
 class Answers extends Component {
-    state = {  }
-    render() { 
-        let {correct, options} = this.props
-        return ( <Segment> 
-            <Grid columns={2} divided>
-              <Grid.Row>
+  state = {}
+  render () {
+    let { correct, options } = this.props
+    return (
+      <Segment>
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Grid column={2}>
+
                 <Grid.Column>
-                  <Grid column={2}>
-                    
-                    <Grid.Column>
-                     <Segment inverted={correct==='a'} color={correct==='a' ? 'green' : null}>{options.a}</Segment>
-                    </Grid.Column>
-                  </Grid>
+                  <Segment
+                    inverted={correct === 'a'}
+                    color={correct === 'a' ? 'green' : null}
+                  >
+                    {' '}<Latex>{options.a}</Latex>
+                  </Segment>
                 </Grid.Column>
+              </Grid>
+            </Grid.Column>
+            <Grid.Column>
+              <Grid column={2}>
+
                 <Grid.Column>
-                  <Grid column={2}>
-                    
-                    <Grid.Column>
-                    <Segment inverted={correct==='b'} color={correct==='b' ? 'green' : null}>{options.b}</Segment>
-                    </Grid.Column>
-                  </Grid>
+                  <Segment
+                    inverted={correct === 'b'}
+                    color={correct === 'b' ? 'green' : null}
+                  >
+                    <Latex>{options.b}</Latex>
+                  </Segment>
                 </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
+              </Grid>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Grid column={2}>
+
                 <Grid.Column>
-                  <Grid column={2}>
-                    
-                    <Grid.Column>
-                    <Segment inverted={correct==='c'} color={correct==='c' ? 'green' : null}>{options.c}</Segment>
-                    </Grid.Column>
-                  </Grid>
+                  <Segment
+                    inverted={correct === 'c'}
+                    color={correct === 'c' ? 'green' : null}
+                  >
+                    <Latex>{options.c}</Latex>
+                  </Segment>
                 </Grid.Column>
+              </Grid>
+            </Grid.Column>
+            <Grid.Column>
+              <Grid column={2}>
+
                 <Grid.Column>
-                  <Grid column={2}>
-                    
-                    <Grid.Column>
-                    <Segment inverted={correct==='d'} color={correct==='d' ? 'green' : null}>{options.d}</Segment>
-                    </Grid.Column>
-                  </Grid>
+                  <Segment
+                    inverted={correct === 'd'}
+                    color={correct === 'd' ? 'green' : null}
+                  >
+                    <Latex>{options.d}</Latex>
+                  </Segment>
                 </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Segment> );
-    }
+              </Grid>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    )
+  }
 }
- 
-export default Answers;
+
+export default Answers
