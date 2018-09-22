18b5937aa963e828a4c8bcf07c1f46ac652f54dd
skiran13 <skiran13@gmail.com>
2018-09-22 22:51:41 +0530

    Added student dash and new test

---
+++ src/front/components/newtest.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import {
+    Sidebar,
+    Segment,
+    Button,
+    Menu,
+    Image,
+    Container,
+    Table,
+    Icon,
+    Header,
+    Input,
+    Grid,
+    Dropdown,
+    Pagination,
+    Modal,
+    GridRow
+  } from 'semantic-ui-react'
+
+  class NewTest extends React.Component{
+      constructor(props){
+          super(props)
+      }
+      render(){
+          return(
+              <div>Hi</div>
+          )
+      }
+  }
+  export default NewTest
