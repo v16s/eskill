d2e11a71677f5c0584510ee07e1c8c443480e485
v16s <shankar.vignesh@gmail.com>
2018-12-16 21:42:20 +0530

    websocket fix #3

--- src/front/App.js
+++ src/front/App.js
@@ -16,7 +16,7 @@ import FacultyDashboard from "./components/FacultyDashboard";
 import QuestionPage from "./components/QuestionPage";
 import ChangeQuestion from "./components/ChangeQuestion";
 import { socket as socUrl } from "./enpoint";
-let socket = io(window.location.origin + '/eskill');
+let socket = io('http://care.srmuniv.ac.in/eskill');
 console.log(window.location.origin + '/eskill')
 import {
   Sidebar,
