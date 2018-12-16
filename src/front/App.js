915c5102d2bc56363c0f47ff4b356cd98e44a5ef
v16s <shankar.vignesh@gmail.com>
2018-12-16 21:38:02 +0530

    fix #2 for socket url

--- src/front/App.js
+++ src/front/App.js
@@ -16,7 +16,7 @@ import FacultyDashboard from "./components/FacultyDashboard";
 import QuestionPage from "./components/QuestionPage";
 import ChangeQuestion from "./components/ChangeQuestion";
 import { socket as socUrl } from "./enpoint";
-let socket = io(socUrl);
+let socket = io(window.location.origin + '/eskill');
 import {
   Sidebar,
   Segment,
