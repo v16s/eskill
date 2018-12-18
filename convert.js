b8d6d29e7dea2f2daab8c436552b1b15495ce8ae
v16s <shankar.vignesh@gmail.com>
2018-12-18 17:49:47 +0530

    update convert.js and add more questions

--- convert.js
+++ convert.js
@@ -11,41 +11,15 @@ const mongoose = require("mongoose");
 const EventEmitter = require("events");
 const Schema = mongoose.Schema;
 const _ = require("lodash");
-const dburl = require('./config.json').dburl;
+const dburl = require("./config.json").dburl;
 const csv = require("csvtojson");
-mongoose.connect(
-  dburl,
-  { useNewUrlParser: true }
-);
+// mongoose.connect(
+//   dburl,
+//   { useNewUrlParser: true }
+// );
 
-let db = mongoose.connection;
+//let db = mongoose.connection;
 
-let Users = mongoose.model(
-  "Users",
-  new Schema({
-    _id: String,
-    email: String,
-    password: String,
-    type: String,
-    level: Number
-  }),
-  "Users"
-);
-let Label = mongoose.model(
-  "Label",
-  new Schema({
-    _id: String,
-    name: String
-  })
-);
-let Tags = mongoose.model(
-  "Tags",
-  new Schema({
-    group: String,
-    name: String
-  }),
-  "Tags"
-);
 let Questions = mongoose.model(
   "Questions",
   new Schema({
@@ -61,90 +35,56 @@ let Questions = mongoose.model(
   }),
   "Questions"
 );
-let UserDetails = mongoose.model(
-  "UserDetails",
-  new Schema({
-    _id: String,
-    details: {
-      name: String,
-      regNo: String,
-      mothersName: String,
-      fathersName: String,
-      dob: Date,
-      gender: String,
-      dateOfAdmission: Date,
-      aadhar: String,
-      passport: String,
-      religion: String,
-      lang: String,
-      deg: String,
-      department: String,
-      courseDuration: String,
-      currentYear: String,
-      academicYear: String
-    }
-  }),
-  "UserDetails"
-);
-let Category = mongoose.model(
-  "Category",
-  new Schema({
-    _id: Number,
-    name: String,
-    topics: Array
-  }),
-  "Category"
-);
-let validateLogin = (acc, content, e) => {
-  console.log(content);
-  if (acc != null) {
-    if (acc.password == content.pass) {
-      e.emit("success", acc);
-    } else {
-      e.emit("fail", "np");
-    }
-  } else {
-    e.emit("fail", "nu");
-  }
-};
+const fs = require("fs");
+
 db.on("open", () => {
   console.log("connected to database");
-
-  csv()
-    .fromFile("./cloud_computing.csv")
-    .then(jsonObj => {
-      jsonObj.map((k, i) => {
-        console.log(i);
-        let obj = {
-          category: {
-            _id: 1,
-            name: "CSE"
-          },
-          label: [],
-          topic: {
-            _id: "102",
-            name: "Cloud Computing"
-          },
-          number: i,
-          answer: k.Answer,
-          options: {
-            a: k["Option A"],
-            b: k["Option B"],
-            c: k["Option C"],
-            d: k["Option D"]
-          },
-          qname: k["Question Name"],
-          qdef: k.Question,
-          hints: k["Explanation"]
-        };
-        let q = new Questions(obj);
-        q.save();
-      });
-      /**
-       * [
-       * 	{a:"1", b:"2", c:"3"},
-       * 	{a:"4", b:"5". c:"6"}
-       * ]
-       */
+  fs.readdir("./questions", (err, files) => {
+    files.map(file => {
+      let filename = file;
+      let name = file.replace("_", " ").split("+");
+      let cid = parseInt(name[1].slice(0, 1)),
+        cname = name[0],
+        tid = name[1],
+        tname = name[2].split(".").shift();
+      console.log(cid, cname, tid, tname);
+      csv()
+        .fromFile("./questions/" + filename)
+        .then(jsonObj => {
+          jsonObj.map((k, i) => {
+            console.log(i);
+            let obj = {
+              category: {
+                _id: cid,
+                name: cname
+              },
+              label: [],
+              topic: {
+                _id: tid,
+                name: tname
+              },
+              number: i,
+              answer: k.Answer,
+              options: {
+                a: k["Option A"],
+                b: k["Option B"],
+                c: k["Option C"],
+                d: k["Option D"]
+              },
+              qname: k["Question Name"],
+              qdef: k.Question,
+              hints: k["Explanation"]
+            };
+            let q = new Questions(obj);
+            q.save();
+          });
+          /**
+           * [
+           * 	{a:"1", b:"2", c:"3"},
+           * 	{a:"4", b:"5". c:"6"}
+           * ]
+           */
+        });
     });
+  });
 });
