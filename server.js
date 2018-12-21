019060da9e3e22ec1a9311f28fccbb7b6b951c2b
v16s <shankar.vignesh@gmail.com>
2018-12-21 20:08:10 +0530

    more changes

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.end("worker: " + cluster.worker.id);
+  res.append("worker: " + cluster.worker.id);
   next();
 });
 app.use("/eskill", express.static(path.resolve(__dirname, "dist")));
