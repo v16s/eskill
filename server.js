837334f68f0ddf8dc1da55c448322cb3a41f463d
v16s <shankar.vignesh@gmail.com>
2018-12-21 20:10:11 +0530

    more fixes

--- server.js
+++ server.js
@@ -73,7 +73,6 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.append("worker: " + cluster.worker.id);
   next();
 });
 app.use("/eskill", express.static(path.resolve(__dirname, "dist")));
